fccab7680451b30668ee32420822883fb165387c
Klyotsa <158348543+Klyotsa@users.noreply.github.com>
2024-10-05 19:33:04 +0300

    Add files via upload

---
+++ 3pract5zad.py
@@ -0,0 +1,7 @@
+def find_minimum(a, b, c):
+    minimum = min(a, b, c)
+    print ("Наименьшее число:", minimum)
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+c = float(input("Введите третье число: "))
+find_minimum(a, b, c)
